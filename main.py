0ff5ad4823c50c588d28182c3bf106a235a5b3ea
Renato <renato92.hora@gmail.com>
2024-08-16 20:28:26 -0300

    fix: correção do travamento da imagem com threading

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import glob
 import os
 from emailing import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(1)
 time.sleep(1)
@@ -51,8 +52,14 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email(image_with_object)
-        clean_folder()
+        email_thread = Thread(target=send_email, args=(image_with_object, )) #args precisa ser uma tupla
+        # send_email(image_with_object)
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        # clean_folder()
+        clean_thread.daemon = True
+
+        email_thread.start()
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
@@ -61,3 +68,6 @@ while True:
         break
 
 video.release()
+
+clean_thread.start()
+
