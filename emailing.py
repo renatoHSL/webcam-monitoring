5a0bb3b7bc5703021d7da20816e441c10f6db4ee
Renato <renato92.hora@gmail.com>
2024-08-16 20:18:42 -0300

    feat: logica de enviar email com foto

--- emailing.py
+++ emailing.py
@@ -1,2 +1,33 @@
-def send_email():
-    print("Email was sent")
+# Não esqueça de configurar na conta do goole gmail:
+# Manage you google account > Security > Turn on 2-step verification
+# Manage you google account > Security > App passwords > Select app > Other (custom name) > "Webcam3" > Salvar password
+# gerado
+
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+PASSWORD = "PASSWORD_GERADO_NO_GMAIL"
+SENDER = "EMAIL QUE SERÁ ENVIADO PARA OUTRO(app8flask@gmail.com)"
+RECEIVER = "EMAIL QUE IRÁ RECEBER (app8flask@gmail.com)"
+
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    email_message.set_content("Hey, we just saw a new customer")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+
+if __name__ == "__main__":
+    send_email()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 import glob
+import os
 from emailing import send_email
 
 video = cv2.VideoCapture(1)
@@ -9,6 +10,12 @@ time.sleep(1)
 first_frame = None
 status_list = []
 count = 1
+
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
 
 while True:
     status = 0
@@ -44,7 +51,8 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_object)
+        clean_folder()
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
